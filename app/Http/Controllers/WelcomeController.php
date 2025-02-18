16df3c95dde7029cb2b388c40d56ecfce8f9f1db
fajrul <fajrulsantoso10@gmail.com>
2025-02-18 13:52:59 +0700

    Jobsheet_2

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello() {
-        return 'Fajrul santoso';
-       }
+    public function hello(){
+        return view('blog.hello', ['name' => 'Bagus']);
+        }
+        public function greeting(){
+            return view('blog.hello')
+            ->with('name','Andi')
+            ->with('occupation','Astronaut');
+            }
+           
        
 }
--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,25 @@ Route::get('/', function () {
 // Route::get('/about', [PageController::class, 'about']);
 // Route::get('/articles/{id}', [PageController::class, 'articles']);
 
-// Route untuk Home
-Route::get('/', HomeController::class);
+// // Route untuk Home
+// Route::get('/', HomeController::class);
 
-// Route untuk About
-Route::get('/about', AboutController::class);
+// // Route untuk About
+// Route::get('/about', AboutController::class);
 
-// Route untuk Artikel dengan ID
-Route::get('/articles/{id}', ArticleController::class);
+// // Route untuk Artikel dengan ID
+// Route::get('/articles/{id}', ArticleController::class);
 
-Route::resource('photos', PhotoController::class);
+// Route::resource('photos', PhotoController::class);
 
-Route::get('/greeting', function () {
-    return view('hello', ['name' => 'fajrul']);
-    });
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'fajrul']);
+//     });
+
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'fajrul']);
+//     });
     
+    Route::get('/greeting', [WelcomeController::class,
+'greeting']);
+
